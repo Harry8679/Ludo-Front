0cce29b4b80f2b9da42cc3c6c19357a5e201da96
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-10 21:22:02 +0200

    Socket Context 2

---
+++ src/contexts/SocketContext.jsx
@@ -0,0 +1,8 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+
+const SocketContext = createContext();
+
+export const useSocket = () => useContext(SocketContext);
+
+export const SocketProvider = 
